d0254f09726bddc8362de42619b072a604c62eb8
gaara <kazekage.kun@gmail.com>
2014-04-22 04:02:03 +0400

    added script for mysql backup (thx ivan4th for this)

---
+++ mysql_backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -u -e
+retcode=0
+host=""
+user=""
+password=""
+dir="$(date +%Y_%m_%d)"
+
+mkdir "$dir"
+for db_name in $(< $1)
+do
+    T="$(date +%Y_%m_%d_%H:%M)"
+    dump_name="${db_name}_$T.sql"
+    echo "db_$db_name backup start time $T"
+    if ! ssh "$host" "mysqldump '$db_name' -u '$user' -p'$password' > '$db_name'_'$T'.sql"; then
+        echo "mysqldump or ssh failed" 1>&2
+        retcode=1
+        continue
+    fi
+    if ! scp -q "$host":"$dump_name" "$dir"; then
+        echo "scp failed" 1>&2
+        retcode=1
+        continue
+    fi
+    echo "$dump_name copying succeeded, removing remote copy..." 1>&2
+    ssh "$host" "rm $dump_name" || true
+    echo "removing completed!" 1>&2
+    echo "db_$db_name backup end time $(date +%Y_%m_%d_%H:%M)" 1>&2
+done
+exit $retcode
